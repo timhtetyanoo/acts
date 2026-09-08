bb226ff22dba87302723acd0cd1b5abefd120c2b
Htet Yan Oo <126285496+timhtetyanoo@users.noreply.github.com>
2026-09-08 13:08:03 +0200

    Add MuonExpandedSector

---
+++ Examples/Framework/include/ActsExamples/EventData/MuonGlobalPatternFinderEvent.hpp
@@ -0,0 +1,138 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 CERN for the benefit of the ACTS project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Acts/Definitions/Algebra.hpp"
+#include "Acts/Utilities/OstreamFormatter.hpp"
+#include "ActsExamples/EventData/MuonSpacePoint.hpp"
+
+#include <array>
+#include <cstdint>
+#include <numbers>
+#include <ostream>
+#include <string>
+#include <vector>
+
+namespace ActsExamples {
+
+namespace MuonSectorMapping {
+constexpr unsigned int s_numSectors = 16;
+constexpr double s_eighthPi = std::numbers::pi / 8.;
+constexpr double s_inverseEighthPi = 8. / std::numbers::pi;
+constexpr std::array<double, 2> s_sectorSize{0.4 * s_eighthPi,
+                                             0.6 * s_eighthPi};
+constexpr double s_sectorOverlap = 0.1 * s_eighthPi;
+
+/// @brief Sector size (exclusive) in radians
+double sectorSize(int sector);
+/// @brief Sector width (with overlap) in radians
+double sectorWidth(int sector);
+/// @brief Returns the central phi position of a sector in radians
+double sectorPhi(int sector);
+/// @brief Transforms a phi position from and to the sector coordinate system
+///        in radians
+double transformPhiToSector(double phi, int sector, bool toSector = true);
+/// @brief Checks whether the phi position is consistent with sector
+bool insideSector(int sector, double phi);
+/// @brief Returns the sector corresponding to the phi position
+int getSector(double phi);
+/// @brief Returns the main sector plus neighboring if the phi position is in
+///        an overlap region
+void getSectors(double phi, std::vector<int>& sectors);
+/// @brief Returns the phi position of the overlap between the two sectors
+///        (which have to be neighboring) in radians
+double sectorOverlapPhi(int sector1, int sector2);
+
+}  // namespace MuonSectorMapping
+
+/// @brief Helper functions to describe the expanded sector concept. The expanded
+///        sectors are based on the 16 fold symmetry of the MS, but also take
+///        into account the overlap between 2 sectors.
+///
+///        The regular msSector is multiplied by 2 and then the sectorProjector
+///        is added which can be either -1 to indicate that the overlap between
+///        the current sector and the left sector is of interest, or 0 to
+///        indicate that the sector center is of interest and finally 1 to
+///        indicate that the sector to the right is of interest.
+class MuonExpandedSector {
+ public:
+  /// @brief Enumeration to select the sector projection of the
+  ///        regular MS sector
+  enum class SectorProjector : std::int8_t {
+    leftOverlap =
+        -1,  ///< Project the segment onto the overlap with the previous sector
+    center = 0,  ///< Project the segment onto the sector centre
+    rightOverlap =
+        1  ///< Project the segment on the overlap with the next sector
+  };
+  /// @brief Return the projector as a string
+  static std::string toString(SectorProjector projector);
+  /// @brief Define the ostream operator
+  friend std::ostream& operator<<(std::ostream& ostr,
+                                  SectorProjector projector) {
+    return ostr << MuonExpandedSector::toString(projector);
+  }
+  friend std::ostream& operator<<(std::ostream& ostr,
+                                  const MuonExpandedSector& sector) {
+    return sector.print(ostr);
+  }
+  /// @brief Constructor of the expanded sector taking the
+  ///        regular MS sector number and the projector
+  /// @param msSector: Number of the ms reference sector [1-16]
+  /// @param proj: Splitting of the sector to the overlap with the
+  ///              left / right adjacent sector or the sector center
+  explicit MuonExpandedSector(unsigned msSector, SectorProjector proj);
+  /// @brief Constructor from an arbitrary phi angle. The angle is
+  ///        assigned to the msSectors and then the expanded sector
+  ///        is deduced
+  /// @param phi: Angle from [-pi, pi]
+  explicit MuonExpandedSector(double phi);
+  /// @brief Constructor from a expanded sector number
+  /// @param expandedSector: Raw expanded sector number
+  explicit MuonExpandedSector(std::int8_t expandedSector);
+  /// @brief Define the ordering operator
+  bool operator<(const MuonExpandedSector& other) const;
+  /// @brief Define the equal operator
+  bool operator==(const MuonExpandedSector& other) const;
+  /// @brief Define the unequal operator
+  bool operator!=(const MuonExpandedSector& other) const;
+  /// @brief Returns the ms sector corresponding to the
+  ///        expanded sector.
+  /// @note If the expanded sector is constructed with the
+  ///       left / right overlap. The msSector number might be
+  ///       the adjacent msSector
+  unsigned msSector() const;
+  /// @brief Returns the neighbouring msSector number constructed from
+  ///        the primary sector and the sector overlap projector
+  unsigned adjacentMsSector() const;
+  /// @brief Returns the projector in the corresponding MS sector
+  SectorProjector projector() const;
+  /// @brief Returns the expanded sector number
+  std::int8_t sector() const;
+  /// @brief Returns the phi angle of the expanded sector
+  double phi() const;
+  /// @brief Returns the vector pointing radially along the sector plane
+  Acts::Vector3 radialDir() const;
+  /// @brief Returns the vector that is normal to the plane spanned
+  ///        by the expanded sector
+  Acts::Vector3 normalDir() const;
+  /// @brief Returns whether the other expanded sector is a neighbour
+  bool isNeighbour(const MuonExpandedSector& other) const;
+
+ private:
+  /// @brief Pipe the object to an ostream
+  std::ostream& print(std::ostream& ostr) const;
+  /// @brief the sector number stored
+  std::int8_t m_sector{0};
+};
+
+}  // namespace ActsExamples
+
+ACTS_OSTREAM_FORMATTER(ActsExamples::MuonExpandedSector::SectorProjector);
+ACTS_OSTREAM_FORMATTER(ActsExamples::MuonExpandedSector);
---
+++ Examples/Framework/src/EventData/MuonGlobalPatternFinderEvent.cpp
@@ -0,0 +1,233 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 CERN for the benefit of the ACTS project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "ActsExamples/EventData/MuonGlobalPatternFinderEvent.hpp"
+
+#include "Acts/Definitions/Units.hpp"
+#include "Acts/Utilities/Helpers.hpp"
+#include "Acts/Utilities/UnitVectors.hpp"
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <stdexcept>
+#include <tuple>
+#include <vector>
+
+using namespace Acts::UnitLiterals;
+
+namespace ActsExamples {
+namespace MuonSectorMapping {
+double sectorSize(int sector) {
+  const int idx = sector % 2;
+  return s_sectorSize[idx];
+}
+double sectorWidth(int sector) {
+  return sectorSize(sector) + s_sectorOverlap;
+}
+double sectorPhi(int sector) {
+  if (sector < 10)
+    return std::numbers::pi * (sector - 1) / 8.;
+  return -std::numbers::pi * (2 - (sector - 1) / 8.);
+}
+double transformPhiToSector(double phi, int sector, bool toSector) {
+  double sign = toSector ? -1 : 1;
+  double dphi = phi + sign * sectorPhi(sector);
+  if (dphi > std::numbers::pi)
+    dphi -= 2 * std::numbers::pi;
+  if (dphi < -std::numbers::pi)
+    dphi += 2 * std::numbers::pi;
+  return dphi;
+}
+bool insideSector(int sector, double phi) {
+  double phiInSec = transformPhiToSector(phi, sector);
+  if (phiInSec < -sectorWidth(sector))
+    return false;
+  if (phiInSec > sectorWidth(sector))
+    return false;
+  return true;
+}
+int getSector(double phi) {
+  // remap phi onto the sector structure
+  double val = (phi + sectorSize(1)) *
+               s_inverseEighthPi;  // convert to value between -8 and 8, shift
+                                   // by width first sector
+  if (val < 0)
+    val += 16;
+  int sliceIndex = static_cast<int>(val / 2);  // large/small wedge
+  double valueInSlice = val - 2 * sliceIndex;
+  int sector = 2 * sliceIndex + 1;
+  if (valueInSlice > 1.2)
+    ++sector;
+  return sector;
+}
+void getSectors(double phi, std::vector<int>& sectors) {
+  int sector = getSector(phi);
+  int sectorNext = sector != 16 ? sector + 1 : 1;
+  int sectorBefore = sector != 1 ? sector - 1 : 16;
+  if (insideSector(sectorBefore, phi))
+    sectors.push_back(sectorBefore);
+  if (insideSector(sector, phi))
+    sectors.push_back(sector);
+  if (insideSector(sectorNext, phi))
+    sectors.push_back(sectorNext);
+}
+double sectorOverlapPhi(int sector1, int sector2) {
+  if (sector1 == sector2)
+    return sectorPhi(sector1);
+
+  int s1 = std::min(sector1, sector2);
+  int s2 = std::max(sector1, sector2);
+  if (s2 == 16 && s1 == 1) {
+    s1 = 16;
+    s2 = 1;
+  } else if (std::abs(s1 - s2) > 1) {
+    return 0;
+  }
+
+  double phi1 = sectorPhi(s1);
+  double phio1 = phi1 + sectorSize(s1);
+  if (phio1 > std::numbers::pi)
+    phio1 -= 2 * std::numbers::pi;
+
+  return phio1;
+}
+}  // namespace MuonSectorMapping
+
+namespace {
+
+using SectorProjector = MuonExpandedSector::SectorProjector;
+
+constexpr std::int8_t s_nExpanded =
+    2 * static_cast<std::int8_t>(MuonSectorMapping::s_numSectors);
+
+std::tuple<unsigned, SectorProjector> msSectorAndProj(
+    std::int8_t expandSector) {
+  assert(expandSector >= 0 && expandSector <= s_nExpanded);
+  if (expandSector == 0) {
+    return {MuonSectorMapping::s_numSectors, SectorProjector::center};
+  } else if (expandSector == 1) {
+    return {MuonSectorMapping::s_numSectors, SectorProjector::rightOverlap};
+  }
+  const int regSector = expandSector / 2;
+  const int backConv = expandSector - 2 * regSector;
+  switch (backConv) {
+    using enum SectorProjector;
+    case 1:
+      return {static_cast<unsigned>(regSector), rightOverlap};
+    case 0:
+      return {static_cast<unsigned>(regSector), center};
+    case -1:
+      return {static_cast<unsigned>(regSector), leftOverlap};
+    default:
+      throw std::invalid_argument(
+          "MuonExpandedSector: cannot deduce the sector overlap");
+  }
+  // all cases have been addressed
+}
+
+}  // namespace
+
+MuonExpandedSector::MuonExpandedSector(unsigned msSector,
+                                       SectorProjector proj) {
+  m_sector = static_cast<std::int8_t>(
+      (2 * msSector + Acts::toUnderlying(proj)) % s_nExpanded);
+}
+
+MuonExpandedSector::MuonExpandedSector(double phi) {
+  std::vector<int> sectors{};
+  MuonSectorMapping::getSectors(phi, sectors);
+  assert(!sectors.empty());
+  if (sectors.size() == 1) {
+    *this = MuonExpandedSector{static_cast<unsigned>(sectors[0]),
+                               SectorProjector::center};
+  } else {
+    const int dS = static_cast<int>((sectors[1] - sectors[0]) %
+                                    MuonSectorMapping::s_numSectors);
+    assert(std::abs(dS) == 1);
+    *this = MuonExpandedSector{static_cast<unsigned>(sectors[0]),
+                               static_cast<SectorProjector>(dS)};
+  }
+}
+
+MuonExpandedSector::MuonExpandedSector(std::int8_t expandedSector)
+    : m_sector{expandedSector} {}
+
+bool MuonExpandedSector::operator<(const MuonExpandedSector& other) const {
+  return sector() < other.sector();
+}
+
+bool MuonExpandedSector::operator==(const MuonExpandedSector& other) const {
+  return sector() == other.sector();
+}
+
+bool MuonExpandedSector::operator!=(const MuonExpandedSector& other) const {
+  return sector() != other.sector();
+}
+
+unsigned MuonExpandedSector::msSector() const {
+  return std::get<0>(msSectorAndProj(sector()));
+}
+
+unsigned MuonExpandedSector::adjacentMsSector() const {
+  const auto [msSec, proj] = msSectorAndProj(sector());
+  if (msSec == 1 && proj == SectorProjector::leftOverlap) {
+    return MuonSectorMapping::s_numSectors;
+  } else if (msSec == MuonSectorMapping::s_numSectors &&
+             proj == SectorProjector::rightOverlap) {
+    return 1;
+  }
+  return msSec + Acts::toUnderlying(proj);
+}
+
+MuonExpandedSector::SectorProjector MuonExpandedSector::projector() const {
+  return std::get<1>(msSectorAndProj(sector()));
+}
+
+std::int8_t MuonExpandedSector::sector() const {
+  return m_sector;
+}
+
+double MuonExpandedSector::phi() const {
+  return MuonSectorMapping::sectorOverlapPhi(msSector(), adjacentMsSector());
+}
+
+Acts::Vector3 MuonExpandedSector::radialDir() const {
+  return Acts::makeDirectionFromPhiTheta(phi(), 90_degree);
+}
+
+Acts::Vector3 MuonExpandedSector::normalDir() const {
+  return Acts::makeDirectionFromPhiTheta(phi() + 90_degree, 90_degree);
+}
+
+bool MuonExpandedSector::isNeighbour(const MuonExpandedSector& other) const {
+  const int dS = (other.sector() - sector()) % s_nExpanded;
+  return std::abs(dS) <= 1;
+}
+
+std::string MuonExpandedSector::toString(SectorProjector projector) {
+  switch (projector) {
+    using enum SectorProjector;
+    case leftOverlap:
+      return "leftOverlap";
+    case center:
+      return "center";
+    case rightOverlap:
+      return "rightOverlap";
+  }
+  return "";
+}
+
+std::ostream& MuonExpandedSector::print(std::ostream& ostr) const {
+  ostr << "Expanded sector: " << static_cast<int>(sector()) << " -> "
+       << projector() << " of sector " << msSector();
+  return ostr;
+}
+
+}  // namespace ActsExamples
