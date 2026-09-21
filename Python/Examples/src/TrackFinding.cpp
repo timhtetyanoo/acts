9392c8ef6134f81bc049603a954bf31e3aed1946
Leonardo Monaco <lmonaco@atlmuonsw1.dyndns.cern.ch>
2026-09-21 17:27:00 +0200

    changes

--- Python/Examples/src/TrackFinding.cpp
+++ Python/Examples/src/TrackFinding.cpp
@@ -112,7 +112,7 @@ void addTrackFinding(py::module& mex) {
   ACTS_PYTHON_DECLARE_ALGORITHM(MuonHoughSeeder, mex, "MuonHoughSeeder",
                                 inTruthSegments, inSpacePoints, outHoughMax,
                                 nBinsTanTheta, nBinsY0, nBinsTanPhi, nBinsX0,
-                                dumpVisualization, visualizationFunction);
+                                dumpVisualization, extendWithPhi, visualizationFunction);
 
   ACTS_PYTHON_DECLARE_ALGORITHM(
       GlobalPatternFinderAlgorithm, mex, "GlobalPatternFinderAlgorithm",
